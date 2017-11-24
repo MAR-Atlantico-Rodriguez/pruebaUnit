3dbbf2b0ab9466d3e734da0c3ef7693b297a5577
Martin Rodriguez <martinrodriguez493@hotmail.com>
2017-11-24 13:29:01 -0300

    ROJO, fallo testSuma y testResta

--- pruebaClass.php
+++ pruebaClass.php
@@ -1,8 +1,28 @@
 <?php
+	class PruebaClass{
 
-class ClasePrueba{
+		public $array = ['foo'=>'algo','otra'=>'cosa'];
 
-   
-}
+	 	public function suma($a,$b) {
+	 	  	return $a+$b;
+	 	}
+
+	 	public function resta($a,$b) {
+	 	  	return $a-$b;
+	 	}
+
+	 	public function multiplicacion($a,$b) {
+	 	  	return $a*$b;
+	 	}
+
+	 	public function comprobarArrayKey(){
+	 		return $this->array;
+	 	}
+
+	 	public function ordenarArray($array){
+	 		sort($array);
+	 		return $array;
+	 	}
+	}
 
 ?>
--- test/testClasePrueba.php
+++ test/testClasePrueba.php
@@ -1,18 +1,44 @@
 <?php
     use PHPUnit\Framework\TestCase;
 
-    use ../pruebaClass
+    use \PruebaClass as PC;
 
     class TestClasePrueba extends TestCase{
+        public $PC;
 
         protected function setUp(){
-            $PC = new pruebaClass();
+            $this->setPC(new PC());
         }
 
         protected function tearDown(){
            
         }
 
+        public function setPC($pc){
+            $this->PC = $pc;
+        }
 
+        public function testSuma(){            
+            $this->assertEquals(10, $this->PC->suma(7,3));
+        }
+        public function testSuma2(){            
+            $this->assertEquals(-1, $this->PC->suma(-0.5,-0.15));
+        }
+
+        public function testResta(){
+            $this->assertEquals(6, $this->PC->resta(7,6));   
+        }
+
+        public function testMultiplicacion(){
+            $this->assertEquals(6, $this->PC->multiplicacion(3,2));   
+        }
+
+        public function testArrayKey(){
+            $this->assertArrayHasKey('otra', $this->PC->comprobarArrayKey());
+        }
+
+        public function testOrder(){
+            $this->assertEquals([1,2,3], $this->PC->ordenarArray([3,1,2]));
+        }
     }
 ?>
