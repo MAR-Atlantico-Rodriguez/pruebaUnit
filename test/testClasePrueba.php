bb01c207c062b4426a3e0f2bdd38f86d5270e453
Martin Rodriguez <martinrodriguez493@hotmail.com>
2017-11-24 10:13:41 -0300

    primer commit

---
+++ test/testClasePrueba.php
@@ -0,0 +1,18 @@
+<?php
+    use PHPUnit\Framework\TestCase;
+
+    use ../pruebaClass
+
+    class TestClasePrueba extends TestCase{
+
+        protected function setUp(){
+            $PC = new pruebaClass();
+        }
+
+        protected function tearDown(){
+           
+        }
+
+
+    }
+?>
